08edac394fd2e25a2c6dfecd0f02e12db27209b3
btmortondev <bmortonbusiness@gmail.com>
2021-10-06 12:20:34 -0400

    Main functionality Update
    Got the main functionality of theme switching implemented.

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+let lightMode = true;
+
+document.querySelector(".check").addEventListener("click", function () {
+  if (lightMode) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    lightMode = false;
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+    lightMode = true;
+  }
+});
